1262a38f94ca5faff94558643f1299fb44504442
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-12-19 09:30:06 +0000

    corrected setup

--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,22 @@ module.exports = {
       template: '!!html-loader!templates/index.html'
     })
   ],
-  devtool: 'sourcemap',
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        use: 'babel-loader'
       },
       {
         test: /\.s?css$/,
         exclude: /node_modules/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        use: 'html-loader'
       },
     ]
   },
